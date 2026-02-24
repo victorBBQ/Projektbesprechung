3f059cc3e0a541006e63b273fbd25d7879d4ad46
Victor Antonio Valenzuela <v.valenzuela@edu.bbq.de>
2026-02-24 12:22:56 +0100

    Module view 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://victorBBQ.github.io',
-  base: '/Projektbesprechung/', 
+  base: '/Projektbesprechung/',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [react()]
 });
